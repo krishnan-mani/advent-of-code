2eeea68c5a91d459d8ad766113f5c71066a7859f
Krishnan M <km@krishnanm.com>
2020-12-18 19:23:43 +0000

    cleaned up sum-of-numbers some more

--- src/advent_of_code/sum_of_numbers.clj
+++ src/advent_of_code/sum_of_numbers.clj
@@ -1,25 +1,22 @@
 (ns advent-of-code.sum-of-numbers
-  (:require [clojure.string :as str])
-  )
+  (:require [clojure.string :as str]))
 
 (defn read-numbers-from-file [filename]
   (map read-string (str/split (slurp filename) #"\n")))
-
-(def numbers (read-numbers-from-file "test/resources/sum_to_2020.txt"))
 
 (defn lazy-contains?
   [coll key]
   (boolean (some #(= % key) coll)))
 
 (def sum 2020)
-
+(def filename "test/resources/sum_to_2020.txt")
+(def numbers (read-numbers-from-file filename))
 (defn -main
   [& args]
   (def first-value (first (filter #(lazy-contains? numbers (- sum %)) numbers)))
   (def second-value (- sum first-value))
   (def solution [first-value second-value])
-  (println (format "%d, %d" first-value second-value))
-  )
+  (println (format "%d, %d" first-value second-value)))
 
 
 
