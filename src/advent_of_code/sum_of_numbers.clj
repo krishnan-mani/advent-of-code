45292ab33cd70c913658a8fbdd759df92f1fc9d9
Krishnan M <km@krishnanm.com>
2020-12-19 21:53:57 +0000

    solved both parts of sum to 2020 problem

--- src/advent_of_code/sum_of_numbers.clj
+++ src/advent_of_code/sum_of_numbers.clj
@@ -12,11 +12,21 @@
   [sum numbers]
   (filter #(lazy-contains? numbers (- sum %)) numbers))
 
+(defn find-three-numbers
+  [sum numbers]
+  (loop [idx 0
+         num (nth numbers idx)
+         tail (rest numbers)]
+    (if (not-empty (find-two-numbers (- sum num) tail))
+      (flatten (list num (find-two-numbers (- sum num) tail)))
+      (recur (inc idx) (nth numbers idx) (rest numbers)))))
+
 (def sum 2020)
 (def filename "test/resources/sum_to_2020.txt")
 (def numbers (read-numbers-from-file filename))
 
 (defn -main
   [& args]
-  (println (find-two-numbers sum numbers)))
-
+  (println (find-two-numbers sum numbers))
+  (println (find-three-numbers sum numbers))
+  )
