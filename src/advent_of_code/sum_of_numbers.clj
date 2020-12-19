ed9b24faed1f39d2751e5661f2ecff2a355e8968
Krishnan M <km@krishnanm.com>
2020-12-19 12:09:49 +0000

    more refactoring

--- src/advent_of_code/sum_of_numbers.clj
+++ src/advent_of_code/sum_of_numbers.clj
@@ -8,28 +8,15 @@
   [coll key]
   (boolean (some #(= % key) coll)))
 
-(def sum 2020)
-(def filename "test/resources/sum_to_2020.txt")
-(def numbers (read-numbers-from-file filename))
-
 (defn find-two-numbers
   [sum numbers]
   (filter #(lazy-contains? numbers (- sum %)) numbers))
 
+(def sum 2020)
+(def filename "test/resources/sum_to_2020.txt")
+(def numbers (read-numbers-from-file filename))
+
 (defn -main
   [& args]
   (println (find-two-numbers sum numbers)))
-
-
-
-
-
-
-
-
-
-
-
-
-
 
