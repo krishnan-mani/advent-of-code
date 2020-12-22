f1e94ac8e55589704ba68764937f44953b16ce40
Krishnan M <km@krishnanm.com>
2020-12-22 20:37:06 +0000

    solved first part of password philosophy

--- src/advent_of_code/password_philosophy.clj
+++ src/advent_of_code/password_philosophy.clj
@@ -5,34 +5,26 @@
 (defn read-passwords-from-file [filename]
   (str/split-lines (slurp filename)))
 
-(defn min-occurences
-  [_string]
+(defn min-occurences [_string]
   (read-string (subs _string 0 1)))
 
-(defn max-occurences
-  [_string]
+(defn max-occurences [_string]
   (read-string (last (str/split (first (str/split _string #" ")) #"-"))))
 
-(defn char-to-test
-  [_string]
-  (last (str/split (first (str/split _string #":")) #" "))
-  )
+(defn char-to-test [_string]
+  (last (str/split (first (str/split _string #":")) #" ")))
 
-(defn get-password
-  [_string]
-  (subs (last (str/split _string #":")) 1)
-  )
+(defn get-password [_string]
+  (subs (last (str/split _string #":")) 1))
 
-(defn occurences
-  [_string chr]
+(defn occurences [_string chr]
   (loop [idx (dec (count _string))
          ans 0]
     (if-not (>= idx 0)
       (+ 0 ans)
       (recur
         (dec idx)
-        (if (= chr (subs _string idx (inc idx))) (inc ans) (+ 0 ans)))
-      )))
+        (if (= chr (subs _string idx (inc idx))) (inc ans) (+ 0 ans))))))
 
 (defn valid? [password min max chr]
   (let [occurences (occurences password chr)]
@@ -41,18 +33,16 @@
       (<= occurences max))
     ))
 
-(defn valid-password-line?
-  [_line]
+(defn valid-password-line? [_line]
   (let [password (get-password _line)
         min (min-occurences _line)
         max (max-occurences _line)
         chr (char-to-test _line)]
     (valid? password min max chr)))
 
-(defn -main
-  [& args]
+(def password-lines (read-passwords-from-file "test/resources/password_policies.txt"))
+(defn -main [& args]
   (println "Number of valid passwords:"
            (count
-             (remove #(if (valid-password-line? %) false true) (read-passwords-from-file "test/resources/password_policies.txt")))
-           )
+             (remove #(if (valid-password-line? %) false true) password-lines)))
   )
