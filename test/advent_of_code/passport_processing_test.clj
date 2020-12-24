7c35a6a501f512a011f0634660128e1d0fab2092
Krishnan M <km@krishnanm.com>
2020-12-24 11:25:48 +0000

    added more tests

--- test/advent_of_code/passport_processing_test.clj
+++ test/advent_of_code/passport_processing_test.clj
@@ -5,8 +5,14 @@
 (def invalid-test-passport
   "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929")
 
+(def another-invalid-test-passport
+  "hcl:#cfa07d eyr:2025 pid:166559648\niyr:2011 ecl:brn hgt:59in")
+
 (def valid-test-passport
   "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm")
+
+(def another-valid-test-passport
+  "hcl:#ae17e1 iyr:2013\neyr:2024\necl:brn pid:760753108 byr:1931\nhgt:179cm")
 
 (def valid-test-passport-data-items
   ["ecl:gry" "pid:860033327" "eyr:2020" "hcl:#fffffd" "byr:1937" "iyr:2017" "cid:147" "hgt:183cm"])
@@ -17,6 +23,8 @@
 (deftest valid-passport?-test
   (is (= true (valid-passport? valid-test-passport)))
   (is (= false (valid-passport? invalid-test-passport)))
+  (is (= true (valid-passport? another-valid-test-passport)))
+  (is (= false (valid-passport? another-invalid-test-passport)))
   )
 
 (deftest get-data-items-test
