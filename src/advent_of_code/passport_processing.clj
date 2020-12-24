8b140048374713ca8d8c088837d833d2ce951204
Krishnan M <km@krishnanm.com>
2020-12-24 11:30:06 +0000

    solved part 1 of passport validation problem

--- src/advent_of_code/passport_processing.clj
+++ src/advent_of_code/passport_processing.clj
@@ -18,3 +18,10 @@
 
 (defn valid-passport? [_passport]
   (has-required-fields? _passport))
+
+(defn read-passports [filename]
+  (str/split (slurp filename) #"\n\n"))
+
+(defn -main [& args]
+  (println "Number of valid passports:"
+           (count (filter identity (map valid-passport? (read-passports "test/resources/passport_batch.txt"))))))
