164b4a4b3dd4275f3c14b0ac73e6117a72e03342
Krishnan Mani <km@krishnanm.com>
2020-12-25 16:48:07 +0000

    checked in missing input file

--- project.clj
+++ project.clj
@@ -3,8 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot advent-of-code.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
-  :plugins [[lein-auto "0.1.2"]])
+  :plugins [[lein-auto "0.1.2"]
+            [jonase/eastwood "0.3.6"]
+            [lein-kibit "0.1.8"]
+            [lein-ancient "0.6.15"]
+            [lein-cloverage "1.1.2"]
+            [lein-nvd "1.4.0"]
+            ])
