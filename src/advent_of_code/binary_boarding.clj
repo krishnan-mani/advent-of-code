d9fb342f3c2670f4c85c059e77fde39eced086ab
Krishnan M <km@krishnanm.com>
2020-12-25 13:06:09 +0000

    determined row number

--- src/advent_of_code/binary_boarding.clj
+++ src/advent_of_code/binary_boarding.clj
@@ -1,10 +1,36 @@
 (ns advent-of-code.binary-boarding
   (:require [clojure.string :as str]))
 
-(defn get-row [_string]
+(defn bisect [range]
+  (let [[lower upper] range
+        half-difference (unchecked-divide-int (- upper lower) 2)]
+    (+ lower half-difference)
+    )
   )
 
+(defn bisect-and-upper [range]
+  (let [mid-point (bisect range)
+        [_ upper] range]
+    [(inc mid-point) upper]
+    )
+  )
 
-(defn bisect [range]
-  (dec (unchecked-divide-int (inc (last range)) 2))
+(defn bisect-and-lower [range]
+  (let [mid-point (bisect range)
+        [lower _] range]
+    [lower mid-point]
+    ))
+
+(defn get-rows [_string]
+  (loop [idx 0
+         range [0 127]
+         ]
+    (if-not (< idx (count _string))
+      range
+      (recur
+        (inc idx)
+        (if (= "B" (str (nth _string idx)))
+          (bisect-and-upper range)
+          (bisect-and-lower range))
+        )))
   )
--- test/advent_of_code/binary_boarding_test.clj
+++ test/advent_of_code/binary_boarding_test.clj
@@ -2,10 +2,36 @@
   (:require [clojure.test :refer :all])
   (:require [advent-of-code.binary-boarding :refer :all]))
 
-(deftest get-row-test
-  (is (= 44 (get-row "FBFBBFF")))
+
+;Start by considering the whole range, rows 0 through 127.
+;F means to take the lower half, keeping rows 0 through 63.
+;B means to take the upper half, keeping rows 32 through 63.
+;F means to take the lower half, keeping rows 32 through 47.
+;B means to take the upper half, keeping rows 40 through 47.
+;B keeps rows 44 through 47.
+;F keeps rows 44 through 45.
+;The final F keeps the lower of the two, row 44.
+
+(deftest get-rows-test
+  (is (= [0 63] (get-rows "F")))
+  (is (= [32 63] (get-rows "FB")))
+  (is (= [32 47] (get-rows "FBF")))
+  (is (= [44 44] (get-rows "FBFBBFF")))
   )
 
 (deftest bisect-test
   (is (= 63 (bisect [0 127])))
+  (is (= 31 (bisect [0 63])))
+  (is (= 47 (bisect [32 63])))
+  )
+
+(deftest bisect-and-upper-test
+  (is (= [64 127] (bisect-and-upper [0 127])))
+  (is (= [32 63] (bisect-and-upper [0 63])))
+  (is (= [40 47] (bisect-and-upper [32 47])))
+  )
+
+(deftest bisect-and-lower-test
+  (is (= [0 63] (bisect-and-lower [0 127])))
+  (is (= [32 47] (bisect-and-lower [32 63])))
   )
