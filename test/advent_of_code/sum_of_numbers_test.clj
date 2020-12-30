1ed71bcccf3f963e5355f48f1ebc0dff76918154
Krishnan Mani <krishnan-mani@users.noreply.github.com>
2020-12-30 12:23:36 +0000

    added acceptance test for sum of numbers (#3)
    Co-authored-by: Krishnan Mani <km@krishnanm.com>

--- test/advent_of_code/sum_of_numbers_test.clj
+++ test/advent_of_code/sum_of_numbers_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all])
   (:require [advent-of-code.sum-of-numbers :refer :all]))
 
-(deftest find-two-numbers-test
+(deftest find-two-numbers-acceptance-test
   (is (= [911 1109] (find-two-numbers sum numbers)))
+  (is (= 1010299 (reduce * (find-two-numbers sum numbers))))
+  )
+
+(deftest find-three-numbers-acceptance-test
+  (is (= [1488 60 472] (find-three-numbers sum numbers)))
+  (is (= 42140160 (reduce * (find-three-numbers sum numbers))))
   )
