5612f77e06a13126d1e5c252efec3877c91f29bd
Krishnan Mani <km@krishnanm.com>
2021-01-07 12:34:22 +0000

    added acceptance tests

--- src/advent_of_code/passport_processing.clj
+++ src/advent_of_code/passport_processing.clj
@@ -1,5 +1,6 @@
 (ns advent-of-code.passport-processing
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:require [clojure.core.match :refer [match]]))
 
 (def optional-fields #{"cid"})
 (def required-fields #{"byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"})
@@ -44,10 +45,10 @@
 (defn valid-height? [_string]
   (let [height (-> _string (str/split colon-pattern) (last))
         num (read-string (re-find #"[\d]*" height))]
-    (or (if (.endsWith height "cm") (within-range? num 150 193))
-        (if (.endsWith height "in") (within-range? num 59 76))
-        false)
-    ))
+    (match [(last (str/split height #"\d"))]
+           ["cm"] (within-range? num 150 193)
+           ["in"] (within-range? num 59 76)
+           :else false)))
 
 (defn valid-hair-color? [_string]
   (let [hair-color (-> _string (str/split,,, colon-pattern) (last))]
